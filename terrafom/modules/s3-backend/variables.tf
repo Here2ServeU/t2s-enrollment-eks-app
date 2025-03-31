c63b38e633b27058e1421d2bdc2131d54b5a07d3
Learn DevOps & Cloud <revnaweji@gmail.com>
2025-03-31 13:38:20 -0500

    Create variables.tf

---
+++ terrafom/modules/s3-backend/variables.tf
@@ -0,0 +1,12 @@
+variable "s3_bucket_name" {
+  description = "S3 bucket name for Terraform state"
+  type        = string
+}
+
+variable "dynamodb_table_name" {
+  description = "DynamoDB table name for state locking"
+  type        = string
+}
+
+
+
