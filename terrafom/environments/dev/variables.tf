921200eed015a6936e4a6c202b167894da534d3e
Learn DevOps & Cloud <revnaweji@gmail.com>
2025-03-31 13:56:58 -0500

    Update variables.tf

--- terrafom/environments/dev/variables.tf
+++ terrafom/environments/dev/variables.tf
@@ -34,4 +34,39 @@ variable "backend_repo" {
   default = "t2s-backend"
 }
 
+variable "datadog_api_key" {
+  description = "Datadog API Key for monitoring"
+  type        = string
+}
+
+variable "frontend_hpa_config" {
+  type = object({
+    name                               = string
+    min_replicas                       = number
+    max_replicas                       = number
+    target_cpu_utilization_percentage = number
+  })
+  default = {
+    name                               = "frontend"
+    min_replicas                       = 2
+    max_replicas                       = 5
+    target_cpu_utilization_percentage = 60
+  }
+}
+
+variable "backend_hpa_config" {
+  type = object({
+    name                               = string
+    min_replicas                       = number
+    max_replicas                       = number
+    target_cpu_utilization_percentage = number
+  })
+  default = {
+    name                               = "backend"
+    min_replicas                       = 2
+    max_replicas                       = 5
+    target_cpu_utilization_percentage = 60
+  }
+}
+
 
