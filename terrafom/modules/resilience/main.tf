a0e83cda1801672c529f5d7dfaff869d08463bd5
Learn DevOps & Cloud <revnaweji@gmail.com>
2025-03-31 13:43:53 -0500

    Update main.tf

--- terrafom/modules/resilience/main.tf
+++ terrafom/modules/resilience/main.tf
@@ -1,31 +1,31 @@
 resource "kubernetes_horizontal_pod_autoscaler" "frontend_hpa" {
   metadata {
-    name = "frontend-hpa"
+    name = "${var.frontend_hpa_config.name}-hpa"
   }
   spec {
     scale_target_ref {
-      kind       = "Deployment"
-      name       = "frontend"
+      kind        = "Deployment"
+      name        = var.frontend_hpa_config.name
       api_version = "apps/v1"
     }
-    min_replicas = 2
-    max_replicas = 5
-    target_cpu_utilization_percentage = 60
+    min_replicas                         = var.frontend_hpa_config.min_replicas
+    max_replicas                         = var.frontend_hpa_config.max_replicas
+    target_cpu_utilization_percentage   = var.frontend_hpa_config.target_cpu_utilization_percentage
   }
 }
 
 resource "kubernetes_horizontal_pod_autoscaler" "backend_hpa" {
   metadata {
-    name = "backend-hpa"
+    name = "${var.backend_hpa_config.name}-hpa"
   }
   spec {
     scale_target_ref {
-      kind       = "Deployment"
-      name       = "backend"
+      kind        = "Deployment"
+      name        = var.backend_hpa_config.name
       api_version = "apps/v1"
     }
-    min_replicas = 2
-    max_replicas = 5
-    target_cpu_utilization_percentage = 60
+    min_replicas                         = var.backend_hpa_config.min_replicas
+    max_replicas                         = var.backend_hpa_config.max_replicas
+    target_cpu_utilization_percentage   = var.backend_hpa_config.target_cpu_utilization_percentage
   }
 }
