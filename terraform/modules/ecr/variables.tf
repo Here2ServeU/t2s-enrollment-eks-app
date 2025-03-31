dced9374fe9a50047eac69af6a011dfd896bc800
Learn DevOps & Cloud <revnaweji@gmail.com>
2025-03-31 15:20:23 -0500

    Delete terraform/modules directory

--- terraform/modules/ecr/variables.tf
+++
@@ -1,7 +0,0 @@
-variable "frontend_repo" {
-  description = "Name of the ECR repository for the frontend"
-}
-
-variable "backend_repo" {
-  description = "Name of the ECR repository for the backend"
-}
--- terraform/modules/s3-backend/variables.tf
+++
@@ -1,7 +0,0 @@
-variable "s3_bucket_name" {
-  description = "S3 bucket name for Terraform state"
-}
-
-variable "dynamodb_table_name" {
-  description = "DynamoDB table name for state locking"
-}
