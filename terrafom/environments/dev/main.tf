72dff149cf836e61990af1295990221f7d670ee7
Learn DevOps & Cloud <revnaweji@gmail.com>
2025-03-31 13:45:29 -0500

    Update main.tf

--- terrafom/environments/dev/main.tf
+++ terrafom/environments/dev/main.tf
@@ -1,12 +1,14 @@
+# terraform/environments/dev/main.tf
+
 module "vpc" {
   source               = "../../modules/vpc"
-  vpc_cidr            = var.vpc_cidr
+  vpc_cidr             = var.vpc_cidr
   public_subnet_cidrs = var.public_subnet_cidrs
-  azs                 = var.azs
+  azs                  = var.azs
 }
 
 module "eks" {
-  source      = "../../modules/eks"
+  source       = "../../modules/eks"
   cluster_name = var.cluster_name
   vpc_id       = module.vpc.vpc_id
   subnet_ids   = module.vpc.public_subnet_ids
@@ -20,11 +22,26 @@ module "s3_backend" {
 }
 
 module "ecr" {
-  source         = "../../modules/ecr"
-  frontend_repo  = var.frontend_repo
-  backend_repo   = var.backend_repo
+  source        = "../../modules/ecr"
+  frontend_repo = var.frontend_repo
+  backend_repo  = var.backend_repo
 }
 
 module "cicd" {
   source = "../../modules/cicd"
+}
+
+module "security" {
+  source = "../../modules/security"
+}
+
+module "monitoring" {
+  source           = "../../modules/monitoring"
+  datadog_api_key  = var.datadog_api_key
+}
+
+module "resilience" {
+  source              = "../../modules/resilience"
+  frontend_hpa_config = var.frontend_hpa_config
+  backend_hpa_config  = var.backend_hpa_config
 }
