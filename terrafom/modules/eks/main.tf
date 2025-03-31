47325432045d689b287989e2e62b4ec2574900ea
Learn DevOps & Cloud <revnaweji@gmail.com>
2025-03-31 13:33:30 -0500

    Create main.tf

---
+++ terrafom/modules/eks/main.tf
@@ -0,0 +1,12 @@
+module "eks" {
+  source          = "terraform-aws-modules/eks/aws"
+  cluster_name    = var.cluster_name
+  cluster_version = "1.27"
+  subnets         = var.subnet_ids
+  vpc_id          = var.vpc_id
+  enable_irsa     = true
+  tags = {
+    Environment = var.environment
+  }
+}
+
