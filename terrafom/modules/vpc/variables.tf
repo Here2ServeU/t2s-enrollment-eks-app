3a6c0c0875981ac663f02e71330f70bc75048cf2
Learn DevOps & Cloud <revnaweji@gmail.com>
2025-03-31 13:32:01 -0500

    Create variables.tf

---
+++ terrafom/modules/vpc/variables.tf
@@ -0,0 +1,15 @@
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "public_subnet_cidrs" {
+  description = "List of public subnet CIDR blocks"
+  type        = list(string)
+}
+
+variable "azs" {
+  description = "List of Availability Zones"
+  type        = list(string)
+}
+
